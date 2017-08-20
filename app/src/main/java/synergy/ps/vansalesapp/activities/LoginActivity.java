6edcef4a0b852035c1a3d2e50116e1a5c4b3ef14
Ahmad Khater <akhater@synergy>
2017-08-20 15:42:25 +0300

    no message

--- app/src/main/java/synergy/ps/vansalesapp/activities/LoginActivity.java
+++ app/src/main/java/synergy/ps/vansalesapp/activities/LoginActivity.java
@@ -1,36 +1,15 @@
 package synergy.ps.vansalesapp.activities;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
+import android.bluetooth.BluetoothAdapter;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import synergy.ps.vansalesapp.R;
-
+import synergy.ps.vansalesapp.utils.BluetoothReceiver;
+import synergy.ps.vansalesapp.utils.PrintManger;
 
 /**
  * A login screen that offers login via email/password.
@@ -38,11 +17,58 @@ import synergy.ps.vansalesapp.R;
 public class LoginActivity extends ParentActivity{
 
 
+
+    // UI references.
+    private EditText username;
+    private EditText password;
+    private Button login;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        BluetoothReceiver bluetoothReceiver=new BluetoothReceiver();
+
+        registerReceiver(bluetoothReceiver, new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+        PrintManger printManager=new PrintManger(getApplicationContext());
+        printManager.findBT();
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PrintManger printManger = new PrintManger(getApplication());
+               if(validateLogin())
+                   printManger.sendData();
+            }
+        });
     }
+
+    public boolean validateLogin() {
+        boolean nameData=true;
+        boolean passData=true;
+        if(username.getText().toString().trim().length()==0){
+            username.setError("Username is not entered");
+            username.requestFocus();
+            nameData=false;
+        }
+        else
+        if(password.getText().toString().trim().length()==0){
+            password.setError("Password is not entered");
+            password.requestFocus();
+            passData=false;
+        }
+        if(nameData && passData)
+            return true;
+        return false;
+
+
+    }
+
 
 }
 
--- app/src/main/java/synergy/ps/vansalesapp/activities/SplashScreenActivity.java
+++ app/src/main/java/synergy/ps/vansalesapp/activities/SplashScreenActivity.java
@@ -1,22 +1,30 @@
 package synergy.ps.vansalesapp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-
-import synergy.ps.vansalesapp.R;
+import android.os.Handler;
 
 public class SplashScreenActivity extends ParentActivity {
-
+    private static int SPLASH_TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 
 
